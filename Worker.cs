4c47d73c82e47d7b1ce0d597c8cdf07d27af6ce7
Sebastian Grönlund <sebastiangronlundwork@gmail.com>
2022-05-20 18:00:40 +0200

    Add project files.

---
+++ Worker.cs
@@ -0,0 +1,53 @@
+using SlackAPI;
+namespace slackBot
+{
+    public class Worker : BackgroundService
+    {
+        private readonly ILogger<Worker> _logger;
+        private readonly string _token;
+        private readonly string _userName;
+        public Worker(ILogger<Worker> logger)
+        {
+            _logger = logger;
+            _token = "xoxb-3544251692277-3558111693236-DkAllRyw4LRaouExnRLFPP8Q";
+            _userName = "sebastiangronlundwork";
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            var slackClient = new SlackTaskClient(_token);
+            var resp = await slackClient.PostMessageAsync("#general", "Starting up....");
+            if (resp == null)
+            {
+                _logger.LogWarning("Startup message failed, failed to connect to workspace");
+            }
+            else
+            {
+                _logger.LogInformation("Bot succesfully started at {stamp}!", DateTimeOffset.Now);
+                var user = await slackClient.GetUserListAsync();
+                if (user != null)
+                {
+                    var members = user.members;
+                    var me = members.FirstOrDefault(m => m.name == _userName);
+                    if(me == null)
+                    {
+                        _logger.LogError("Couldn't find user: {0}", _userName);
+                    } else
+                    {
+                        var userDM = await slackClient.JoinDirectMessageChannelAsync(me.real_name);
+                        if(userDM == null)
+                        {
+                            _logger.LogError("Couldn't join DM");
+                        } else
+                        {
+                            var userChannel = userDM.channel;
+                            await slackClient.PostMessageAsync(userChannel.id, "Hello from the DMs!");
+                        }
+
+                    }
+                }
+            }
+            
+        }
+    }
+}
